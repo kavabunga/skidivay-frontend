bd0bed7dfc2cfcdb13a7cdbfb8494023a3baf861
Iosif Kireev <44925378+kirei-io@users.noreply.github.com>
2023-10-25 18:23:07 +0300

    refactor/project-structure (#1)
    * docs: добавлены правила оформления веток и пуллреквестов
    
    * refactor: изменение сборщика проекта на vite
    
    * refactor: линтинг и форматирование
    
    * style: форматирование
    
    * refactor: структура проекта

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,19 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/preset-create-react-app',
 		'@storybook/addon-onboarding',
 		'@storybook/addon-interactions',
 	],
 	framework: {
-		name: '@storybook/react-webpack5',
+		name: '@storybook/react-vite',
 		options: {},
 	},
 	docs: {
 		autodocs: 'tag',
 	},
-	staticDirs: ['../public'],
 };
 export default config;
