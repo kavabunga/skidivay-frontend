5d6a9aaf96977c316c477d6dda699407bfc4a1cb
kavabunga <kavabunga@gmail.com>
2023-11-02 22:16:01 +0200

    refactor: refactor mui button comp, add storybook examples

--- src/shared/ui/Input-mui/index.tsx
+++ src/shared/ui/Input-mui/index.tsx
@@ -1,22 +1,7 @@
-import { FC } from 'react';
 import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
 
-interface InputMUIProps {
-	isEnabled: boolean;
-	error: boolean;
-	name: string;
-	label: string;
-	startAdornment: boolean;
-}
-
-const InputMUI: FC<InputMUIProps> = ({ isEnabled, label, ...props }) => {
-	return (
-		<div>
-			<InputLabel htmlFor="input-with-icon-adornment">{label}</InputLabel>
-			<TextField disabled={!isEnabled} {...props}></TextField>
-		</div>
-	);
+const InputMUI = ({ ...props }) => {
+	return <TextField {...props} />;
 };
 
 export default InputMUI;
--- src/stories/input.stories.ts
+++ src/stories/input.stories.ts
@@ -8,29 +8,52 @@ const meta = {
 		layout: 'centered',
 	},
 	tags: ['autodocs'],
+	argTypes: {
+		label: {
+			options: ['Email', 'Пароль', 'Имя', 'Телефон'],
+			control: { type: 'radio' },
+		},
+		required: {
+			control: { type: 'boolean' },
+		},
+		variant: {
+			options: ['outlined', 'filled', 'standard'],
+			control: { type: 'radio' },
+		},
+		helperText: {
+			control: { type: 'text' },
+			default: '',
+		},
+		type: {
+			options: ['password', 'number', 'search', 'text'],
+			control: { type: 'radio' },
+		},
+		defaultValue: {
+			control: { type: 'text' },
+		},
+	},
 } satisfies Meta<typeof InputMUI>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+	args: {},
+};
+export const Password: Story = {
 	args: {
-		value: 'Value',
-		isEnabled: true,
+		label: 'password',
+		required: true,
+		variant: 'outlined',
+		type: 'password',
 	},
 };
-
-export const Disable: Story = {
+export const Name: Story = {
 	args: {
-		value: 'Value',
-		isEnabled: false,
-	},
-};
-
-export const Error: Story = {
-	args: {
-		value: 'Value',
-		isEnabled: true,
-		error: true,
+		label: 'Имя',
+		required: true,
+		variant: 'outlined',
+		type: 'text',
+		helperText: 'Как к вам обращаться?',
 	},
 };
