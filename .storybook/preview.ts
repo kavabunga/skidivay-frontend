0170784b4dcdc64813cf009931b0f0fe1129f3e3
kavabunga <kavabunga@gmail.com>
2023-11-05 01:07:50 +0200

    fix: more formatted files

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,41 +6,41 @@ import { lightTheme } from '../src/shared/lib';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const customViewports = {
-	kindleFire2: {
-		name: 'mobile',
-		styles: {
-			width: '360px',
-			height: '963px',
-		},
-	},
+  kindleFire2: {
+    name: 'mobile',
+    styles: {
+      width: '360px',
+      height: '963px',
+    },
+  },
 };
 
 const preview: Preview = {
-	parameters: {
-		actions: { argTypesRegex: '^on[A-Z].*' },
-		parameters: {
-			viewport: {
-				viewports: { ...INITIAL_VIEWPORTS, ...customViewports },
-			},
-		},
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
-	},
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    parameters: {
+      viewport: {
+        viewports: { ...INITIAL_VIEWPORTS, ...customViewports },
+      },
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
 };
 
 export const decorators = [
-	withThemeFromJSXProvider({
-		themes: {
-			light: lightTheme,
-		},
-		defaultTheme: 'light',
-		GlobalStyles: CssBaseline,
-		Provider: ThemeProvider,
-	}),
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+    },
+    defaultTheme: 'light',
+    GlobalStyles: CssBaseline,
+    Provider: ThemeProvider,
+  }),
 ];
 
 export default preview;
--- src/features/default-layout/ui/index.tsx
+++ src/features/default-layout/ui/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Container from '@mui/material/Container';
 
 interface DefaultLayoutProps {
-	header: React.ReactNode;
-	children: React.ReactNode;
+  header: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const DefaultLayout = ({ header, children }: DefaultLayoutProps) => {
-	return (
-		<>
-			<Container>
-				{header}
-				{children}
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        {header}
+        {children}
+      </Container>
+    </>
+  );
 };
