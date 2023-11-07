0c7ea0e5c104fe540b594c16861a044d4b8f5976
Helga Milne <kiko-riko@yandex.ru>
2023-11-07 23:55:42 +0300

    feat: fix to resolve merge

---
+++ src/shared/ui/account-button/style.ts
@@ -0,0 +1,19 @@
+type SxProps = {
+  padding: string;
+  color: string;
+  fontSize: string;
+  borderSize: string;
+  borderStyle: string;
+  borderColor: string;
+  borderRadius: string;
+};
+
+export const style: SxProps = {
+  padding: '0.75rem',
+  color: 'primary.main',
+  fontSize: '1rem',
+  borderSize: '1px',
+  borderStyle: 'solid',
+  borderColor: 'surface.main',
+  borderRadius: '0.5rem',
+};
