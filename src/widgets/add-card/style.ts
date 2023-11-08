765d5c2daf50c9737f21bf9eaa8a3eeb237b9d8a
kavabunga <kavabunga@gmail.com>
2023-11-09 01:21:54 +0200

    feat: add new card widget
    Добавить виджет для добавления новой карты
    
    closes #54

---
+++ src/widgets/add-card/style.ts
@@ -0,0 +1,24 @@
+import { SxProps } from '@mui/material';
+
+export const containerStyle: SxProps = {
+  display: 'flex',
+  flexDirection: 'column',
+  paddingX: '30px',
+  paddingTop: '8px',
+  paddingBottom: '30px',
+};
+
+export const titleStyle: SxProps = {
+  fontSize: 32,
+  lineHeight: 1.25,
+  fontWeight: 400,
+  paddingTop: '40px',
+  paddingBottom: '20px',
+};
+
+export const backButtonStyle: SxProps = {
+  alignSelf: 'flex-start',
+  fontSize: 12,
+  fontWeight: 500,
+  lineHeight: 1.33,
+};
