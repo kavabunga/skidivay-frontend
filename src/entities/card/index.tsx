92d0842cfbdea8409885a09edc302310450f991e
Helga Milne <kiko-riko@yandex.ru>
2023-11-08 11:10:11 +0300

    fix: fix according to the review

--- src/entities/card/index.tsx
+++ src/entities/card/index.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
 import { useState } from 'react';
-import { Box } from '@mui/material';
-import { Paper } from '@mui/material';
-import { IconButton } from '@mui/material';
+import { Box, Paper, IconButton } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { boxStyle, iconButtonStyle } from './styles';
 
 type CardProps = {
   card: {
@@ -15,8 +14,15 @@ type CardProps = {
     category: string;
     isLiked: boolean;
   };
-  onClickCard(): void;
-  onClickLike(): void;
+  onClickCard(name: string, number: string, url: string): void;
+  onClickLike(card: {
+    _id: string;
+    name: string;
+    number: string;
+    url: string;
+    category: string;
+    isLiked: boolean;
+  }): void;
 };
 
 export const Card: FC<CardProps> = ({ card, onClickCard, onClickLike }) => {
@@ -33,30 +39,10 @@ export const Card: FC<CardProps> = ({ card, onClickCard, onClickLike }) => {
 
   return (
     <>
-      <Paper
-        elevation={2}
-        sx={{
-          borderRadius: '20px',
-        }}
-      >
+      <Paper elevation={2} sx={{ borderRadius: '20px' }}>
         <Box
           component="img"
-          sx={{
-            display: 'block',
-            width: '100%',
-            aspectRatio: '1 / 0.63',
-            backgroundColor: '#f6f6f6',
-            objectFit: 'cover',
-            objectPosition: 'center',
-            borderRadius: '20px',
-            overflow: 'hidden',
-            cursor: 'pointer',
-            filter: 'brightness(100%)',
-            transition: 'filter 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-            '&:hover': {
-              filter: 'brightness(90%)',
-            },
-          }}
+          sx={{ ...boxStyle }}
           alt={`скидочная карта от ${card.name}`}
           src={card.url || '#'}
           onClick={handleClick}
@@ -64,18 +50,7 @@ export const Card: FC<CardProps> = ({ card, onClickCard, onClickLike }) => {
       </Paper>
       <IconButton
         aria-label="heart symbol"
-        sx={{
-          position: 'absolute',
-          top: '1rem',
-          right: '1rem',
-          color: 'primary.light',
-          filter: 'invert(0%)',
-          zIndex: '2',
-          transition: 'filter 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
-          '&:hover': {
-            filter: 'invert(100%)',
-          },
-        }}
+        sx={{ ...iconButtonStyle }}
         onClick={handleLikeClick}
       >
         {card.isLiked ? (
--- src/widgets/cards-list/index.tsx
+++ src/widgets/cards-list/index.tsx
@@ -1,18 +1,40 @@
+import { FC } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Card } from '~/entities';
 import { AddCardButton } from '~/features';
+import { defaultCards } from '~/shared/mock/default-cards';
 
-export const CardsList = ({
-  items = cards,
-  onClickCard,
-  onClickLike,
-  onClickAddCard,
-}) => {
+type CardProps = {
+  _id: string;
+  name: string;
+  number: string;
+  url: string;
+  category: string;
+  isLiked: boolean;
+};
+
+type CardsListProps = {
+  items: CardProps[];
+};
+
+export const CardsList: FC<CardsListProps> = ({ items = defaultCards }) => {
+  function handleClickCard(name: string, number: string, url: string): void {
+    console.log(`card ${name}, ${number}, ${url}, is clicked`);
+  }
+
+  function handleClickLike(card: CardProps): void {
+    console.log(`card ${card} is liked`);
+  }
+
+  function handleAddCard(): void {
+    console.log('card is added');
+  }
+
   return (
     <Grid
       container
       spacing={2}
-      columns={{ xs: 12, sm: 12, md: 12 }}
+      columns={{ xs: 12 }}
       justifyContent="flex-start"
       alignItems="center"
     >
@@ -29,7 +51,7 @@ export const CardsList = ({
         }}
       >
         <AddCardButton
-          onClick={onClickAddCard}
+          onClick={handleAddCard}
           text="Добавить новую&nbsp;карту"
         />
       </Grid>
@@ -50,8 +72,8 @@ export const CardsList = ({
           >
             <Card
               card={item}
-              onClickCard={onClickCard}
-              onClickLike={onClickLike}
+              onClickCard={handleClickCard}
+              onClickLike={handleClickLike}
             />
           </Grid>
         );
@@ -59,165 +81,3 @@ export const CardsList = ({
     </Grid>
   );
 };
-
-const cards: {
-  _id: string;
-  name: string;
-  number: string;
-  url: string;
-  category: string;
-  isLiked: boolean;
-}[] = [
-  {
-    _id: ' h54k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/h42qWNnk/cloth-shoes-01.jpg',
-    category: 'cloth-shoes',
-    isLiked: false,
-  },
-  {
-    _id: ' h54kfg5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/GhTg7fmJ/office-01.jpg',
-    category: 'office',
-    isLiked: true,
-  },
-  {
-    _id: ' h5sd5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/1XkB1930/entertainments-01.jpg',
-    category: 'entertainments',
-    isLiked: false,
-  },
-  {
-    _id: ' h54shk5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/Kzdpx79g/appliances-01.jpg',
-    category: 'appliances',
-    isLiked: false,
-  },
-  {
-    _id: ' h54974k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/PqBZ3S39/pharmacies-01.jpg',
-    category: 'pharmacies',
-    isLiked: false,
-  },
-  {
-    _id: ' 6774k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/8PGw4zxD/household-products-01.jpg',
-    category: 'household-products',
-    isLiked: false,
-  },
-  {
-    _id: ' hwwek5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/fbVvytS7/cloth-shoes-05.png',
-    category: 'cloth-shoes',
-    isLiked: false,
-  },
-  {
-    _id: ' h5dd4k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/50wLQw4z/construction-products-02.png',
-    category: 'construction-products',
-    isLiked: false,
-  },
-  {
-    _id: ' h5458k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/L8cJkn03/sport-01.jpg',
-    category: 'sport',
-    isLiked: true,
-  },
-  {
-    _id: ' h54sdomvk5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/kgJm09RP/cloth-shoes-03.png',
-    category: 'cloth-shoes',
-    isLiked: false,
-  },
-  {
-    _id: ' h549v4k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/jSdp8XBs/construction-products-01.png',
-    category: 'construction-products',
-    isLiked: true,
-  },
-  {
-    _id: ' h5458k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/d0ygDm3v/sport-02.jpg',
-    category: 'sport',
-    isLiked: true,
-  },
-  {
-    _id: ' h5f3494k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/XJzSFysV/food-02.jpg',
-    category: 'food',
-    isLiked: false,
-  },
-  {
-    _id: ' hht54k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/L4GhFqmK/entertainments-02.png',
-    category: 'entertainments',
-    isLiked: false,
-  },
-  {
-    _id: ' h604554k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/8CyPKcZY/appliances-03.png',
-    category: 'appliances',
-    isLiked: false,
-  },
-  {
-    _id: ' h5407k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/023jxzx1/cosmetics-perfumery-01.jpg',
-    category: 'cosmetics-perfumery',
-    isLiked: false,
-  },
-  {
-    _id: ' h52854k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/mgYsqkgw/optics-01.jpg',
-    category: 'optics',
-    isLiked: true,
-  },
-  {
-    _id: ' h5f3494k5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/d3L2tnQj/food-01.png',
-    category: 'food',
-    isLiked: false,
-  },
-  {
-    _id: ' h54dmik5j',
-    name: ' Лавка',
-    number: '123',
-    url: 'https://i.postimg.cc/FRMv8L3T/household-products-02.png',
-    category: 'household-products',
-    isLiked: true,
-  },
-];
