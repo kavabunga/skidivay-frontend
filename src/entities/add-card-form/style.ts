1447105df298da2ab91f5fadbcc9122fbf63f3cf
kavabunga <kavabunga@gmail.com>
2023-11-08 23:03:14 +0200

    feat: add new card form
    closes #58 closes #51

---
+++ src/entities/add-card-form/style.ts
@@ -0,0 +1,31 @@
+import { SxProps } from '@mui/material';
+
+export const formStyle: SxProps = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '12px',
+  paddingTop: '12px',
+};
+
+export const textInputStyle: SxProps = {};
+
+export const helperTextStyle: SxProps = {
+  fontSize: 12,
+  fontWeight: 400,
+  lineHeight: 1.3,
+  minHeight: 20,
+};
+
+export const listBoxStyle: SxProps = {
+  bgcolor: '#F3EDF7',
+  fontSize: 16,
+  fontWeight: 400,
+  lineHeight: 1.5,
+};
+
+export const buttonStyle: SxProps = {
+  fontSize: 14,
+  fontWeight: 500,
+  lineHeight: 1.4,
+  paddingY: 2.25,
+};
