070ac74b7ea79bade197bbf816c68c667a6a1c60
Helga Milne <kiko-riko@yandex.ru>
2023-11-09 17:24:24 +0300

    fix(shared/ui): fix accent-button

--- src/shared/ui/accent-button/style.ts
+++ src/shared/ui/accent-button/style.ts
@@ -2,7 +2,6 @@ import { SxProps } from '@mui/material';
 
 export const style: SxProps = {
   width: '100%',
-  minWidth: '18rem',
   maxWidth: '48rem',
   fontSize: '0.875rem',
   lineHeight: '1.25rem',
