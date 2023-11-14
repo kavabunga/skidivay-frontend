02ee62b17047298430215b1f9c6b9a28cbda4fb3
Helga Milne <kiko-riko@yandex.ru>
2023-11-14 15:47:13 +0300

    refactor: add path "/authorizedNoCards" and  path "/authorizedWithCards"

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,10 +2,23 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthLayout, NotFound, RootLayout } from '~/pages';
-import { Home, AuthWidget, CardWidget, AddCardWidget } from '~/widgets';
+import {
+  Home,
+  AuthWidget,
+  CardWidget,
+  AddCardWidget,
+  Welcome,
+  UserCards,
+} from '~/widgets';
+import { defaultCards } from '~/shared/mock/default-cards';
+import { chipsLabels } from '~/shared/mock/chips-labels';
 import { lightTheme } from '~/shared/lib';
 import 'typeface-roboto';
 import 'typeface-nunito';
+
+function handleLogOut() {
+  console.log('user logged out');
+}
 
 export function App() {
   return (
@@ -18,6 +31,20 @@ export function App() {
           </Route>
           <Route path="/" Component={RootLayout}>
             <Route index Component={Home} />
+            <Route
+              path="authorizedNoCards"
+              element={<Welcome user={{ name: 'Василий' }} />}
+            />
+            <Route
+              path="authorizedWithCards"
+              element={
+                <UserCards
+                  cards={defaultCards}
+                  tags={chipsLabels}
+                  logOut={handleLogOut}
+                />
+              }
+            />
             <Route path="card">
               <Route path="new" Component={AddCardWidget} />
               <Route path=":id" Component={CardWidget} />
--- src/widgets/home/index.tsx
+++ src/widgets/home/index.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import { AccentButton } from '~/shared/ui';
 import { PromoSlider } from '~/features';
@@ -7,24 +6,6 @@ import { defaultPromoCards } from '~/shared/mock';
 import { coverImgStyle, mainContainerStyle, paragraphStyle } from './styles';
 
 export const Home = () => {
-  const [width, setWidth] = useState(document.documentElement.clientWidth);
-
-  useEffect(() => {
-    setWidth(document.documentElement.clientWidth);
-    function handleResize() {
-      setWidth(document.documentElement.clientWidth);
-    }
-    function wait() {
-      let timeoutId: ReturnType<typeof setTimeout>;
-      return function () {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(handleResize, 1000);
-      };
-    }
-    window.addEventListener('resize', wait());
-    return () => window.removeEventListener('resize', wait());
-  }, [width]);
-
   return (
     <Container component="main" sx={{ ...mainContainerStyle }}>
       <Typography
