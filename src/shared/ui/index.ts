f31473bbb8384291fb41829de6b8e7eb6c7f629b
kavabunga <kavabunga@gmail.com>
2023-11-16 01:19:47 +0200

    fix: fix logo import

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,6 @@
 export * from './app-footer';
 export * from './close-button';
-export * from './logo';
+export * from './logotype';
 export * from './account-button';
 export * from './chip-button';
 export * from './promo-card';
