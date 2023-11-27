37d354fa7768b6d37525f06408c72938659c96cb
Helga Milne <kiko-riko@yandex.ru>
2023-11-27 17:32:14 +0300

    refactor: apply async/await for liker click

--- src/features/liker/index.tsx
+++ src/features/liker/index.tsx
@@ -1,5 +1,6 @@
 import { FC, useContext } from 'react';
 import { CardsContext } from '~/app/contexts';
+import { ICardContext } from '~/shared/types';
 import { api } from '~/shared';
 import { IconButton } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -14,18 +15,19 @@ interface LikerProps {
 export const Liker: FC<LikerProps> = ({ cardId, isLiked }) => {
   const { cards, setCards } = useContext(CardsContext);
 
-  function handleClick() {
-    cards &&
-      setCards &&
-      api
-        .changeCardLikeStatus(cardId, !isLiked)
-        .then((newCard) => {
-          const newCards = cards.map((item) =>
-            item.card.id === cardId ? newCard : item
-          );
-          setCards(newCards);
-        })
-        .catch((err) => console.log(err));
+  async function handleClick() {
+    if (cards && setCards) {
+      let newCard: ICardContext;
+      try {
+        newCard = await api.changeCardLikeStatus(cardId, !isLiked);
+      } catch (e) {
+        console.log(e);
+      }
+      const newCards = cards.map((item) =>
+        item.card.id === cardId ? newCard : item
+      );
+      setCards(newCards);
+    }
   }
 
   return (
