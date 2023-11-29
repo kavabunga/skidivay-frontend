c0ede63a9614bb2728743c3a9c7b61a9bbb0c494
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-11-29 23:09:11 +0300

    feat: now options for search taken from user cards and sorting starts after user input

--- src/features/search-line/ui/index.tsx
+++ src/features/search-line/ui/index.tsx
@@ -1,9 +1,10 @@
 import { Autocomplete, TextField } from '@mui/material';
-import React, { useContext } from 'react';
-import { CardsContext, ShopListContext, SortedCardsContext } from '~/app';
+import React, { useContext, useEffect, useState } from 'react';
+import { CardsContext, SortedCardsContext } from '~/app';
 
 export const SearchLine = () => {
-  const { shops } = useContext(ShopListContext);
+  const [options, setOptions] = useState<string[]>([]);
+
   const { cards } = useContext(CardsContext);
   const { setSortedCards } = useContext(SortedCardsContext);
   function onChange(e: React.SyntheticEvent) {
@@ -17,12 +18,46 @@ export const SearchLine = () => {
     }
   }
 
+  function onInput(e: React.BaseSyntheticEvent) {
+    const newCards = cards.filter((card) => {
+      return card.card.shop?.name
+        .toLowerCase()
+        .startsWith(e.target.value.toLowerCase().trim());
+    });
+
+    if (newCards.length) {
+      return setSortedCards && setSortedCards(newCards);
+    } else {
+      return setSortedCards && setSortedCards(cards);
+    }
+  }
+
+  useEffect(() => {
+    const optionsArray: string[] = [];
+    cards.forEach((card) => {
+      if (card.card.shop) {
+        if (!optionsArray.includes(card.card.shop.name)) {
+          optionsArray.push(card.card.shop.name);
+        }
+      }
+    });
+
+    setOptions(optionsArray);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Autocomplete
       fullWidth
       onChange={(e) => onChange(e)}
-      options={shops ? shops.map((option) => option.name) : ['']}
-      renderInput={(params) => <TextField {...params} placeholder="Поиск" />}
+      options={options ? options.map((option) => option) : ['']}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder="Поиск"
+          onChange={(e) => onInput(e)}
+        />
+      )}
     />
   );
 };
