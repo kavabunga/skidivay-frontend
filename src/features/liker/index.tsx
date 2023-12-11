ab963bf324aafe5b10f497542fd300c3d45fb47d
kavabunga <kavabunga@gmail.com>
2023-12-11 19:29:17 +0200

    refactor: add error handling in Liker
    Убрал console.log и добавил обработку ошибок в Liker

--- src/features/liker/index.tsx
+++ src/features/liker/index.tsx
@@ -1,11 +1,12 @@
 import { FC, useContext, MouseEvent } from 'react';
-import { CardsContext } from '~/app/contexts';
-import { ICardContext } from '~/shared/types';
+import { CardsContext, MessagesContext } from '~/app/contexts';
 import { api } from '~/shared';
 import { IconButton } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { iconButtonStyle } from './style';
+import { IApiError } from '~/shared/errors';
+import { ApiMessageTypes } from '~/shared/enums';
 
 interface LikerProps {
   cardId: number;
@@ -14,22 +15,33 @@ interface LikerProps {
 
 export const Liker: FC<LikerProps> = ({ cardId, isLiked }) => {
   const { cards, setCards } = useContext(CardsContext);
+  const { setMessages } = useContext(MessagesContext);
 
-  async function handleClick(e: MouseEvent) {
+  const handleError = (err: IApiError) => {
+    setMessages((messages) => [
+      {
+        message:
+          err.detail?.non_field_errors?.join(' ') ||
+          err.message ||
+          'Ошибка сервера',
+        type: ApiMessageTypes.error,
+      },
+      ...messages,
+    ]);
+  };
+
+  function handleClick(e: MouseEvent) {
     //NOTE: Cancelling event bubbling to prevent firing card click
     e.stopPropagation();
-    if (cards && setCards) {
-      let newCard: ICardContext;
-      try {
-        newCard = await api.changeCardLikeStatus(cardId, !isLiked);
-      } catch (e) {
-        console.log(e);
-      }
-      const newCards = cards.map((item) =>
-        item.card.id === cardId ? newCard : item
-      );
-      setCards(newCards);
-    }
+    api
+      .changeCardLikeStatus(cardId, !isLiked)
+      .then((res) => {
+        const newCards = cards.map((item) =>
+          item.card.id === cardId ? res : item
+        );
+        setCards && setCards(newCards);
+      })
+      .catch(handleError);
   }
 
   return (
