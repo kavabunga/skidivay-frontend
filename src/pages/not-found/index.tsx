4b8033a2608d43c818ceed866ff748400eb87cfc
Darya <120787941+kolotilina-d@users.noreply.github.com>
2023-12-16 22:11:56 +0200

    Update index.tsx

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,9 +1,26 @@
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+import mainImage from '~/shared/assets/not-found-robot.svg';
+import { mainImgStyle } from './styles.ts';
+import { AccentButton } from '~/shared/index.ts';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <Stack direction="column" sx={{ paddingX: '1rem' }}>
-      <Typography>Эта страница не найдена</Typography>
+      <Typography sx={{ paddingY: '2rem' }} textAlign="center">
+        Cтраница не найдена
+      </Typography>
+      <Box
+        component="img"
+        sx={{ ...mainImgStyle }}
+        alt="Робот, с ошибкой 404 на дисплее"
+        src={mainImage}
+      />
+      <AccentButton sx={{ marginBottom: '4rem' }} onClick={() => navigate(-1)}>
+        Вернуться назад
+      </AccentButton>
     </Stack>
   );
 };
