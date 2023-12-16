95f4daf78836e6a7b5b79a0127cb68ee9bdc5910
Darya <120787941+kolotilina-d@users.noreply.github.com>
2023-12-16 22:12:38 +0200

    Create styles.ts

---
+++ src/pages/not-found/styles.ts
@@ -0,0 +1,13 @@
+import { SxProps } from '@mui/material';
+
+export const mainImgStyle: SxProps = {
+  display: 'block',
+  width: '100%',
+  minWidth: '20rem',
+  minHeight: '15rem',
+  aspectRatio: '1 / 0.75',
+  objectFit: 'contain',
+  objectPosition: 'center',
+  flexShrink: '0',
+  padding: '2rem 0'
+};
