e51678804f0f053ac4eeef8aa356b2a5bf335eac
Helga Milne <kiko-riko@yandex.ru>
2023-12-18 21:53:08 +0300

    fix: set media query dependency for hover

--- src/features/liker/style.ts
+++ src/features/liker/style.ts
@@ -3,9 +3,6 @@ import { SxProps } from '@mui/material';
 export const iconButtonLightStyle: SxProps = {
   color: '#FFFBFF',
   padding: '0',
-  '&:hover': {
-    filter: 'invert(100%)',
-  },
   '& .MuiSvgIcon-root': {
     fontSize: '1.5rem',
   },
@@ -24,15 +21,52 @@ export const iconButtonLightStyle: SxProps = {
       fontSize: '2rem',
     },
   },
+  '@media(hover: hover) and (pointer: fine)': {
+    '&:hover': {
+      color: 'primary.main',
+    },
+  },
+  '@media(any-hover: hover) and (any-pointer: fine)': {
+    '&:hover': {
+      color: 'primary.main',
+    },
+  },
+  '@media(hover: none) and (any-hover: none)': {
+    '&:active': {
+      color: '#FFFBFF',
+    },
+  },
+  '@media(pointer: coarse) and (any-pointer: coarse) and (any-pointer: none)': {
+    '&:active': {
+      color: '#FFFBFF',
+    },
+  },
 };
 
 export const iconButtonDarkStyle: SxProps = {
   color: 'surface.dark',
   padding: '0',
-  '&:hover': {
-    filter: 'invert(100%)',
-  },
   '& .MuiSvgIcon-root': {
     fontSize: '1.5rem',
+  },
+  '@media(hover: hover) and (pointer: fine)': {
+    '&:hover': {
+      color: 'primary.main',
+    },
+  },
+  '@media(any-hover: hover) and (any-pointer: fine)': {
+    '&:hover': {
+      color: 'primary.main',
+    },
+  },
+  '@media(hover: none) and (any-hover: none)': {
+    '&:active': {
+      color: 'surface.dark',
+    },
+  },
+  '@media(pointer: coarse) and (any-pointer: coarse) and (any-pointer: none)': {
+    '&:active': {
+      color: 'surface.dark',
+    },
   },
 };
